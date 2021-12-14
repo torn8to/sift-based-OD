8fd64ac1ff4a25cc7cfcc15488a4dbe6e1791764
Keval Shah <kevalshah278@gmail.com>
2021-12-14 15:42:29 -0500

    Update Appended_AP.py

--- Appended_AP.py
+++ Appended_AP.py
@@ -2,6 +2,7 @@ from AffineParameters import *
 
 appended_affine_parameters = []
 
+# Calculates the Affine Parameters for all the bins
 for i in range(0, count):
     appended_affine_parameters.append(AffineParameters(i))
 
